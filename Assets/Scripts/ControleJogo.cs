b5fad38e62c97183389830b3b88f2d8efbf2f0fd
EvioMarcio <eviomarcio.silva@gmail.com>
2018-07-27 16:51:30 -0300

    Programação do GamePlay criação de pontes infinitas

--- Assets/Scripts/ControleJogo.cs
+++ Assets/Scripts/ControleJogo.cs
@@ -11,13 +11,19 @@ public class ControleJogo : MonoBehaviour {
     public float limiteXMaximo;
     public float limiteXMinimo;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Header("Configuração da GamePlay")]
+    public float velocidadeObjetos;
+    public float tamanhaPrefabPonte;
+
+    [Header("Configuração dos Prefabs")]
+    public GameObject prefabPonte;
+    public GameObject prefabBarril;
+
+	public void InstanciarPonte(float posicaoX)
+    {
+        GameObject tempPonte = Instantiate(prefabPonte);
+        tempPonte.transform.position = new Vector3(posicaoX + tamanhaPrefabPonte, tempPonte.transform.position.y, 0);
+    }
+
+
 }
--- Assets/Scripts/ControlePonte.cs
+++ Assets/Scripts/ControlePonte.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class ControlePonte : MonoBehaviour {
 
+    private ControleJogo controleJogo;
+    private Rigidbody2D rBody;
+
+    private bool instanciado;
+
 	// Use this for initialization
 	void Start () {
-		
+
+        controleJogo = FindObjectOfType(typeof(ControleJogo)) as ControleJogo;
+
+        rBody = GetComponent<Rigidbody2D>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        rBody.velocity = new Vector2(controleJogo.velocidadeObjetos, 0);
+
+        if (transform.position.x <= 0 && instanciado == false)
+        {
+            instanciado = true;
+            controleJogo.InstanciarPonte(transform.position.x);
+        }
+
+        if (transform.position.x <= -8)
+        {
+            Destroy(this.gameObject);
+        }
+
 	}
 }
