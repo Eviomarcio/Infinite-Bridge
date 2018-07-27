7b84537706642cc5df6439792e6bc2656540f3cd
EvioMarcio <eviomarcio.silva@gmail.com>
2018-07-27 13:17:00 -0300

    Programação da movimentação das Montanhas e Nuvens

--- Assets/Scripts/MoveOffSet.cs
+++ Assets/Scripts/MoveOffSet.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class MoveOffSet : MonoBehaviour {
 
+    private Renderer renderizadorMalha;
+    private Material materialAtual;
+    private float offSet;
+    public int ordenRenderizacao;
+    public float  incrementoOffSet, velocidade;
+
 	// Use this for initialization
 	void Start () {
-		
+        renderizadorMalha = GetComponent<Renderer>();
+        renderizadorMalha.sortingOrder = ordenRenderizacao;
+        materialAtual = renderizadorMalha.material;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        offSet += incrementoOffSet;
+        materialAtual.SetTextureOffset("_MainTex", new Vector2(offSet * velocidade, 0));
+
 	}
 }
